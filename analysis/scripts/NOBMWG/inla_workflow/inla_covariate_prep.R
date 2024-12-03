a676416330b2de8aa5e360bff9a22967d94d3d85
Sarah Endicott <sarah.endicott@ec.gc.ca>
2024-12-03 10:40:34 -0500

    Add gee covariate extraction

--- analysis/scripts/NOBMWG/inla_workflow/inla_covariate_prep.R
+++ analysis/scripts/NOBMWG/inla_workflow/inla_covariate_prep.R
@@ -2,7 +2,10 @@ library(sf)
 library(tidyverse)
 library(terra)
 library(exactextractr)
+library(data.table) # used in gee_points_extraction
+library(rgee)
 
+devtools::load_all(".")
 # Get data from previous scripts
 Study_Area_bound <- read_sf("analysis/data/derived_data/INLA_data/study_area.gpkg")
 
@@ -61,7 +64,8 @@ nalcms_2020_Can <- reproducible::Cache(reproducible::prepInputs(
   useCache = TRUE
 ))
 
-terra::writeRaster(nalcms_2020_Can, "analysis/data/derived_data/covariates/NALCMS_NorthON.tif")
+terra::writeRaster(nalcms_2020_Can, "analysis/data/derived_data/covariates/NALCMS_NorthON.tif",
+                   overwrite = TRUE)
 
 url_adaptwest_norm <- "https://s3-us-west-2.amazonaws.com/www.cacpd.org/CMIP6v73/normals/Normal_1961_1990_bioclim.zip"
 
@@ -153,21 +157,109 @@ all_surveys_1km <- bind_cols(all_surveys_1km, prop_LCC_1km) %>%
 all_surveys_covariates <- left_join(all_surveys_covariates, all_surveys_1km,
                                     by = join_by(Obs_Index))
 
+# Do GEE covariate extraction #=================================================
+visit <- all_surveys %>%
+  sf::st_drop_geometry() %>%
+  # rename to fit WT format
+  dplyr::mutate(
+    project_id = Project_Name, location_id = survey_ID,
+    latitude = Latitude, longitude = Longitude,
+    year = lubridate::year(Date_Time), .keep = "none"
+  )
+
+visit_yr <- visit %>%
+  dplyr::select(project_id, location_id, latitude, longitude, year) %>%
+  dplyr::mutate(year = as.integer(year)) %>%
+  unique() %>%
+  dplyr::mutate(id = paste(project_id, location_id, latitude, longitude, year, sep = "_")) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326, remove = FALSE) %>%
+  st_transform(crs = 5072)
+
+BAM_var_list <- read.csv(file.path("analysis/data/raw_data/", "Model_VariableList.csv"))
+
+# To force download of all data set Complete to 0
+# BAM_var_list$Complete <- 0
+#
+# write.csv(BAM_var_list, file.path("analysis/data/raw_data/", "Model_VariableList.csv"),
+#           row.names = FALSE)
+
+# initialize rgee
+ee$Authenticate(auth_mode='notebook')
+ee$Initialize(project='ee-sarahendicott-eccc')  # <-- EDIT THIS FOR YOUR PROJECT
+
+# Optionally
+# make a request to verify you are connected.
+ee$String('Hello from the Earth Engine servers!')$getInfo()
+
+
+# takes around 3 hours to run
+tictoc::tic()
+# Function uses GEE and the BAM variables list to extract BAM variables that are
+# available from GEE including for year matched time series variables
+gee_points_extract(meth_path = file.path("analysis/data/raw_data/", "Model_VariableList.csv"),
+                   loc.yr = visit_yr,
+                   out_save = "analysis/data/derived_data/INLA_data/survey_covariates")
+
+tictoc::toc()
+
+static_dat <- read.csv("analysis/data/derived_data/INLA_data/survey_covariates/data_covariates_GEE_static.csv")
+match_dat <- read.csv("analysis/data/derived_data/INLA_data/survey_covariates/data_covariates_GEE_match.csv")
+
+# Here following the national models code:
+# Remove lat lon fields due to rounding errors that cause mismatches
+# Format landcover classes as factors
+# Zero out heights < 0.1 and NA the height cv values for those
+# Remove 0 values for some layers that should be NAs
+# Format landcover classes as factors
+# Create eBird method and make method a factor
+BAM_var_list <- read.csv(file.path("analysis/data/raw_data/", "Model_VariableList.csv"))
+
+meth.use <- BAM_var_list %>%
+  dplyr::filter(Complete==1)
+
+# TODO: Steps for variables not yet extracted are commented out below. These
+# should really be programmed from the variables spreadsheet so that they can be
+# applied even when some variables are missing
+visit.covs <- visit %>%
+  mutate(id=paste(project_id, location_id, latitude, longitude, year, sep="_"),
+         year=as.integer(year)) %>%
+  left_join(static_dat %>% select(-c(latitude, longitude))) %>%
+  left_join(match_dat %>% select(-c(latitude, longitude))) %>%
+  dplyr::select(-id) %>%
+  dplyr::select(all_of(colnames(visit)), any_of(meth.use$Label)) %>%
+  mutate(#across(c(NLCD_1km, VLCE_1km),~ifelse (.x == 0, NA, .x)),
+         across(c(#SCANFIheight_1km, SCANFIheight_5x5,
+                  ETHheight_1km, ETHheight_5x5),
+                  #LFheigth_1km, LFheigth_5x5),
+                ~ifelse (.x < 0.1, 0, .x)),
+         #SCANFIheightcv_1km = ifelse (SCANFIheight_1km < 0.1, NA, SCANFIheightcv_1km),
+         #SCANFIheightcv_5x5= ifelse (SCANFIheight_5x5 < 0.1, NA, SCANFIheightcv_5x5),
+         ETHheightcv_1km = ifelse (ETHheight_1km < 0.1, NA, ETHheightcv_1km),
+         ETHheightcv_5x5 = ifelse (ETHheight_5x5 < 0.1, NA, ETHheightcv_5x5),
+         #LFheigthcv_1km = ifelse (LFheigth_1km < 0.1, NA, LFheigthcv_1km),
+         #LFheigthcv_5x5= ifelse (LFheigth_5x5 < 0.1, NA, LFheigthcv_5x5),
+         #Test this
+         # across(c(SCANFIheightcv_1km, SCANFIheightcv_5x5, ETHheightcv_1km, ETHheightcv_5x5,
+         #         LFheigthcv_1km, LFheigthcv_5x5), ~ifelse (c(SCANFIheight_1km,
+         #         SCANFIheight_5x5, ETHheight_1km, ETHheight_5x5, LFheigth_1km,
+         #         LFheigth_5x5) < 0.1, NA, .x))
+         across(c(MODISLCC_1km, MODISLCC_5x5),
+                  #SCANFI_1km, NLCD_1km,
+                  #ABoVE_1km, VLCE_1km),
+                as.factor))
 
 # Extract grid covariates within 1 km of location #=============================
 
-# TODO: consider whether it is really helpful to have this as vector or if it
-# could be a raster?
 # Continuous covariates
 ONGrid_1km <- ONGrid_centroid %>%
   st_buffer(1000) %>%
   mutate(#elevation_1km = exact_extract(elevation, ., 'mean'),
-    AMT_1km = exact_extract(covars$mat, ., 'mean'))#,
+    AMT_1km = exact_extract(mat_SA, ., 'mean'))#,
 # SCC_1km = exact_extract(SCC, ., 'mean'))
 
 # TODO: avoid this repetition
 # Proportion of each land cover class
-prop_LCC_1km <- exact_extract(covars$NALCMS, ONGrid_1km,"frac") %>% suppressWarnings()
+prop_LCC_1km <- exact_extract(nalcms_2020_Can, ONGrid_1km,"frac") %>% suppressWarnings()
 names(prop_LCC_1km) <- paste0(str_replace(names(prop_LCC_1km),"frac","LCC"),"_1km")
 prop_LCC_1km[setdiff(paste0("LCC_",seq(1,18),"_1km"),names(prop_LCC_1km))] <- 0
 prop_LCC_1km <- prop_LCC_1km %>% dplyr::select(sort(names(.)))
@@ -187,8 +279,39 @@ prop_LCC_1km <- prop_LCC_1km %>%
 
 ONGrid_1km <- bind_cols(ONGrid_1km, prop_LCC_1km)
 
+# Re-initialize covariate list for grid extraction.
+# To force download of all data set Complete to 0
+BAM_var_list$Complete <- 0
+
+write.csv(BAM_var_list, file.path("analysis/data/raw_data/", "Model_VariableList.csv"),
+          row.names = FALSE)
+tictoc::tic()
+gee_points_extract(meth_path = file.path("analysis/data/raw_data/", "Model_VariableList.csv"),
+                   loc.yr = ONGrid_centroid %>% mutate(year = 1, id = point_id),
+                   year_get = "max",
+                   out_save = "analysis/data/derived_data/INLA_data/grid_covariates")
+tictoc::toc()
+
+static_grid_dat <- read.csv("analysis/data/derived_data/INLA_data/grid_covariates/data_covariates_GEE_static.csv")
+match_grid_dat <- read.csv("analysis/data/derived_data/INLA_data/grid_covariates/data_covariates_GEE_match.csv")
+
+# same as above but dropped the commented out parts.
+ONGrid_gee_covs <- ONGrid_centroid %>%
+  left_join(static_grid_dat, by = join_by(point_id)) %>%
+  left_join(match_grid_dat %>% select(-id, -year), by = join_by(point_id)) %>%
+  dplyr::select(-id) %>%
+  dplyr::select(all_of(colnames(ONGrid_centroid)), any_of(meth.use$Label)) %>%
+  mutate(across(c(ETHheight_1km, ETHheight_5x5),
+                ~ifelse (.x < 0.1, 0, .x)),
+         ETHheightcv_1km = ifelse (ETHheight_1km < 0.1, NA, ETHheightcv_1km),
+         ETHheightcv_5x5 = ifelse (ETHheight_5x5 < 0.1, NA, ETHheightcv_5x5),
+         across(c(MODISLCC_1km, MODISLCC_5x5),
+                as.factor))
+
+
 # Join with ONGrid
-ONGrid <- ONGrid %>% left_join(st_drop_geometry(ONGrid_1km), by = join_by(point_id))
+ONGrid <- ONGrid %>% left_join(st_drop_geometry(ONGrid_1km), by = join_by(point_id)) %>%
+  left_join(st_drop_geometry(ONGrid_gee_covs), by = join_by(point_id))
 
 # Covariate PCA #===============================================================
 
--- analysis/scripts/NOBMWG/inla_workflow/inla_workflow_notes.md
+++ analysis/scripts/NOBMWG/inla_workflow/inla_workflow_notes.md
@@ -12,7 +12,7 @@ Waiting on final list of variables to use. In the mean time I have incorporated
 BAM's method for getting variables from Google Earth Engine (GEE) with David's
 existing covariate extraction from rasters.
 
-### Issues
+### Issues/suggestions
   - For categorical variables BAM uses the mode to get value but in David's we
   use proportion of the polygon with each land cover class. Differences will be
   especially pronounced for larger radius. It looks like the
@@ -21,7 +21,15 @@ existing covariate extraction from rasters.
   https://gis.stackexchange.com/questions/415445/calculate-coverage-of-class-types-in-gee
 
   - Don't know how BAM gets their prediction rasters. David currently makes
-  prediction grids in vector form. Could modify BAM code to download whole
-  rasters or use David's method of getting values from grid centre point and
-  combine that with BAM's method for getting raster values in a radius around a
-  point. The later would require new code to just get the most recent year.
+  prediction grids in vector form. Could modify BAM GEE extraction code to download whole
+  rasters. I have used David's method of getting values from prediction grid centre point and
+  combined that with BAM's method for getting raster values in a radius around a
+  point and modified it to use the most recent year for predictions. Currently
+  radius is set in the spreadsheet but not sure if that makes sense if we use
+  those prediction for mapping
+
+  - Various conversions for covariates are hard coded into the covariate
+  assembly step. It would be better if columns were added to the spreadsheet to
+  define these post extraction steps so the code could run without all the
+  variables.
+
